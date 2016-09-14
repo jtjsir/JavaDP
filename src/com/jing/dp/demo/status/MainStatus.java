ae90c69bddfefc545d068713b76d759bd9847f84
jtjsir <15958042648@163.com>
2016-09-14 15:44:08 +0800

    新增状态模式

---
+++ src/com/jing/dp/demo/status/MainStatus.java
@@ -0,0 +1,16 @@
+package com.jing.dp.demo.status;
+
+/**
+ * @author jingsir
+ **
+ *         状态模式：主要针对的是某个对象的行为取决于其自身的状态，并且该对象的状态会在运行期间进行相应的切换
+ *         一定程度上可以减少if/else的语句
+ */
+public class MainStatus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
