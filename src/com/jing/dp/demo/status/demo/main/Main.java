63f8a0181f7695d0a29a37b373daa94e5a40ddca
jtjsir <15958042648@163.com>
2018-02-27 16:48:33 +0800

    add the output result in the status sample

--- src/com/jing/dp/demo/status/demo/main/Main.java
+++ src/com/jing/dp/demo/status/demo/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         Context context = new Context();
-        // 设置状态为开梯操作
+        // 设置状态为关梯
         context.setLiftState(Context.CLOSESTATE);
         // 一次完整的上下电梯操作
         context.open();
@@ -18,5 +18,15 @@ public class Main {
         context.stop();
         context.open();
         context.close();
+
+        /**
+         * 输入结果如下
+         *
+         *the door is opening!
+         *the door is closing
+         *the lift is running
+         *the lift is stopping
+         *
+         */
     }
 }
